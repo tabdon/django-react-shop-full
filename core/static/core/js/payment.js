129bbf9262c10baf1e299b62489aada85b0a842a
tobias <tabdon@gmail.com>
2015-09-28 09:45:46 +0700

    Product Purchases - Create the Payment Form Component

--- core/static/core/js/payment.js
+++ core/static/core/js/payment.js
@@ -51,3 +51,131 @@ Validator.prototype.validateExpiry = function(value) {
 
     return Stripe.validateExpiry(month.toString(), year.toString());
 };
+
+var PaymentForm = React.createClass({
+    componentDidMount: function() {
+        this.loadingElement = jQuery(this.getDOMNode()).find('.loading');
+        this.loadingElement.hide();
+        this.isLoading = false;
+    },
+
+    // displayMessage method
+    displayMessage: function(css, message) {
+        var messageEl = jQuery(this.getDOMNode()).find('.message');
+        messageEl.addClass(css);
+        messageEl.text(message);
+    },
+
+    // handleSubmit method
+    handleSubmit: function(e) {
+        e.preventDefault();
+        if (!this.isLoading) {
+            this.isLoading = true;
+            this.loadingElement.show();
+
+            var formData = {
+                amount: this.props.product.price
+            };
+
+            var hasErrors = false;
+
+            var self = this;
+            var validator = new Validator();
+            jQuery(this.getDOMNode()).find('.form-group').each(function (i, el) {
+                var group = $(el);
+                var formElement = group.find(':input');
+                if (formElement.val() != '') {
+                    var attrName = formElement.attr('name');
+
+                    if (validator.validationMap.hasOwnProperty(attrName)) {
+                        var func = validator.validationMap[attrName];
+
+                        var isValid = false;
+
+                        if (typeof func === 'function') {
+                            isValid = func(formElement.val());
+                        } else {
+                            isValid = validator[func](formElement.val());
+                        }
+
+                        if (!isValid) {
+                            group.addClass('has-error');
+                            hasErrors = true;
+                            return false; // continue to the next iteration
+                        }
+                    }
+
+                    group.removeClass('has-error');
+                    formData[attrName] = formElement.val();
+                } else {
+                    group.addClass('has-error');
+                    hasErrors = true;
+                }
+            });
+
+            if (!hasErrors) {
+                formData.expiry = validator.parseExpiry(formData.expiry);
+                var apiService = new APIService();
+                apiService.makePayment(this.props.product, formData, function(data) {
+                    self.displayMessage('text-success', 'Payment Successful');
+                    self.isLoading = false;
+                    self.loadingElement.hide();
+                }, function(error) {
+                    self.displayMessage('text-danger', self.generateErrorText(response.data));
+                    self.isLoading = false;
+                    self.loadingElement.hide();
+                });
+
+            } else {
+                this.isLoading = false;
+                this.loadingElement.hide();
+            }
+        }
+    },
+
+    // render method
+    render: function() {
+        return (
+            <form name="form" className="form-horizontal" noValidate>
+                <div className="form-group">
+                    <label className="control-label col-sm-2 col-sm-offset-1">Full Name</label>
+                    <div className="col-sm-8">
+                        <input type="text" name="name" className="form-control" placeholder="Required" required/>
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-3">E-mail Address</label>
+                    <div className="col-sm-8">
+                        <input type="email" name="email" className="form-control" placeholder="Required" required/>
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-3">Card Number</label>
+                    <div className="col-sm-8">
+                        <input type="text" maxLength="20" name="card" autoComplete="off" className="form-control" required />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-3">CVC</label>
+                    <div className="col-sm-8">
+                        <input type="text" maxLength="4" name="cvc" autoComplete="off" className="form-control" required />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label col-sm-3">Expiry Date</label>
+                    <div className="col-sm-8">
+                        <input type="text" className="form-control" name="expiry" required />
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary pull-left" onClick={this.handleSubmit}>Buy for ${this.props.product.price}</button>
+                <div className="loading pull-left"></div>
+                <div className="message pull-left"></div>
+            </form>
+        )
+    }
+
+});
