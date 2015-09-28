9ec24346c5582b4c0064fda5fea2492520fe1f68
tobias <tabdon@gmail.com>
2015-09-28 08:08:39 +0700

    Product Detail Page - Create Product Detail Component

---
+++ core/static/core/js/productdetail.js
@@ -0,0 +1,33 @@
+var ProductDetail = React.createClass({
+    getInitialState: function() {
+        return {};
+    },
+
+    // componentDidMount method
+    componentDidMount: function() {
+        var api = new APIService();
+
+        api.getProduct(this.props.productId, function(data) {
+            this.setState(data);
+        }.bind(this));
+    },
+
+    // renderModal method
+
+    // render method
+    render: function() {
+        return (
+            <article className="product clearfix">
+                <div className="col-md-6">
+                    <img className="img-thumbnail" src={this.state.image} />
+                </div>
+                <div className="col-md-6">
+                    <h1>{this.state.name}</h1>
+                    <h4 className="mfg">by {this.state.mfg}</h4>
+                    <div className="description">{this.state.desc}</div>
+                    <button type="button" className="btn btn-primary" onClick={this.renderModal}>Buy Now ${this.state.price}</button>
+                </div>
+            </article>
+        )
+    }
+});
